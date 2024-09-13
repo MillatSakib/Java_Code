8ddc55c25c210ce9dac6754d67b3e25d926a4338
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-09-13 11:02:46 +0600

    Loop completed

---
+++ General/Loop/DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;  // Initialize counter
+
+        // Using a do-while loop to print numbers from 1 to 10
+        do {
+            System.out.println("Number: " + i);
+            i++;  // Increment counter after each iteration
+        } while (i <= 10);  // Condition to continue the loop
+    }
+}
---
+++ General/Loop/While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+        int i = 1;  // Initialize counter
+
+        // Using a while loop to print numbers from 1 to 10
+        while (i <= 10) {
+            System.out.println("Number: " + i);
+            i++;  // Increment counter after each iteration
+        }
+    }
+}
