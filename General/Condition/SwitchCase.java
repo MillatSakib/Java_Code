c3256a4cdc203808c9adda9d1dc8940cc8d72055
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-09-13 10:55:26 +0600

    Switch case completed

---
+++ General/Condition/SwitchCase.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class SwitchCase {
+    public static void main(String[] args){
+        int x;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        x = input.nextInt();
+        switch (x){
+            case 0:
+                System.out.println("X is zero");
+                break;
+            case 1:
+                System.out.println("X is one");
+                break;
+            case 2:
+                System.out.println("X is two");
+                break;
+
+            case 3:
+                System.out.println("X is three");
+                break;
+            case 4:
+                System.out.println("X is four");
+                break;
+            default:
+                System.out.println("X is unidentified.");
+        }
+    }
+}
